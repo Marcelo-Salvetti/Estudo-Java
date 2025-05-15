6721d23a6ee32b14ee1f49c6bca2b0a30998d4b9
Marcelo Luiz Salvetti <luizmarcelosalvetti@gmail.com>
2025-05-14 23:03:38 -0300

    Realocção

--- Java-Script/03.js
+++
@@ -1,9 +0,0 @@
-/* Prof. Robson Martins
-   TADS - Anhanguera
-   Web/JS - Estruturas Condicionais
-   
-   03. Use um operador ternário para verificar se um número é par ou ímpar.
-*/
-
-let numero = 7;
-console.log(numero % 2 === 0 ? "Par" : "Ímpar");
